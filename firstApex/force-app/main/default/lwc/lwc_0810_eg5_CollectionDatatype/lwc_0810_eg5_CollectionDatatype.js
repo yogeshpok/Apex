d33ebda5acaa7e0b2b3d5a1bfb0360f558be3a2e
Yogesh Pokharkar <yogesh_pokharkar@epam.com>
2025-12-22 17:27:59 +0530

    collection

---
+++ firstApex/force-app/main/default/lwc/lwc_0810_eg5_CollectionDatatype/lwc_0810_eg5_CollectionDatatype.js
@@ -0,0 +1,37 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwc_0810_eg5_CollectionDatatype extends LightningElement {
+
+    cities =['hyd','mum','pune','delhi','pune'] //array
+
+
+    student ={
+        'name' :'yogesh pok',
+        'age' : 12,
+        'salary' :232455
+    }
+    
+
+    students =[{
+        'name' :'YOGI',
+        'age' : 12,
+        'salary' :232455
+    },
+    {
+        'name' :'SUNNY',
+        'age' : 12,
+        'salary' :232455
+    },
+    {
+        'name' :'LCOKY',
+        'age' : 12,
+        'salary' :232455
+    },
+    {
+        'name' :'SHOKY',
+        'age' : 12,
+        'salary' :232455
+    }]
+
+
+}
